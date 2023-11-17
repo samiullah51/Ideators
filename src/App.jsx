94410cdbb18c191f97688b8818d34a74608ebfe2
samifiver32 <samifiver51728@gmail.com>
2023-11-18 01:41:33 +0500

    voice added

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,63 @@
-import { useState } from "react";
-import HomePage from "./pages/HomePage";
-import CardsPage from "./pages/CardsPage";
-import SignUp from "./pages/SignUp";
-import Signin from "./pages/Signin";
-import Verify from "./pages/Verify";
-import { Routes, Route } from "react-router-dom";
-function App() {
+import { AudioRecorder, useAudioRecorder } from "react-audio-voice-recorder";
+import { useState, useEffect, useRef } from "react";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+import "./App.css";
+export default function App() {
+  const recorderControls = useAudioRecorder();
+  const [audioURL, setAudioURL] = useState("");
+  const totalTimeRef = useRef(0);
+
+  const getFile = (blob) => {
+    console.log("file");
+    console.debug("getting file: ", blob);
+    console.debug("recorderControls: ", {
+      blob: recorderControls.recordingBlob,
+      time: recorderControls.recordingTime,
+    });
+    const modifiedBlob = new Blob([blob], { type: "audio/ogg" });
+    console.debug("modifiedBlob: ", blob);
+
+    const url = URL.createObjectURL(modifiedBlob);
+    setAudioURL(url);
+  };
+
+  function formatTime(seconds) {
+    var hours = Math.floor(seconds / 3600);
+    var minutes = Math.floor((seconds % 3600) / 60);
+    var remainingSeconds = seconds % 60;
+
+    var formattedTime =
+      hours.toString().padStart(2, "0") +
+      ":" +
+      minutes.toString().padStart(2, "0") +
+      ":" +
+      remainingSeconds.toString().padStart(2, "0");
+
+    return formattedTime;
+  }
+
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/cards" element={<CardsPage />} />
-        <Route path="/register" element={<SignUp />} />
-        <Route path="/verify" element={<Verify />} />
-        <Route path="/login" element={<Signin />} />
-      </Routes>
-      {/* <HomePage /> */}
-      {/* <CardsPage /> */}
-      {/* <SignUp /> */}
-      {/* <Verify /> */}
-      {/* <Signin /> */}
-    </>
+    <div className="App">
+      <AudioRecorder
+        className="customAudioRecorder" // Apply custom class name
+        recorderControls={recorderControls}
+        onRecordingComplete={getFile}
+        showVisualizer
+        visualizationCanvasClassName="customVisualizerCanvas"
+      />
+      <button onClick={() => setAudioURL("")}>cancel</button>
+      {audioURL && (
+        <AudioPlayer
+          className="myCustomAudioPlayer" // Add a custom class name
+          defaultDuration={formatTime(totalTimeRef.current)}
+          src={audioURL}
+          autoPlay={false}
+          showJumpControls={false}
+          volumeJumpStep={false}
+          showFilledVolume={false}
+        />
+      )}
+    </div>
   );
 }
-
-export default App;
